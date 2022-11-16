9cb55fce95fe314c7d979f26c60d1c5a065906ab
adrian14 <A68899@eon.com>
2022-11-16 18:43:31 +0100

    init

---
+++ variables.py
@@ -0,0 +1,14 @@
+## Variables eshte nje emer i cili i referohet nje vlere
+
+
+message = 'Ky eshte nje mesazh'
+print(message)
+
+n = 23
+print(n)
+
+pi = 3.14
+pint(pi)
+
+delta = 1
+print(delta)
---
+++ vlerat.py
@@ -0,0 +1,15 @@
+## Gjej tipin e vlerave
+type("Python")
+type(003A3)
+type([3,3,4])
+type(1)
+type(true)
+
+
+
+## Printo Vlerat
+print(1)
+
+print(23)
+
+print("Ckemi")
